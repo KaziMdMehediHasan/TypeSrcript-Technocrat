e8f164f428fe6fa187155e8a5115697716638ec3
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-04-30 19:49:11 +0600

    basic typescript data types upto tuples

---
+++ module1/src/1.4.ts
@@ -0,0 +1,29 @@
+// basic data type
+
+// string
+
+let firstName: string = 'Quazi';
+
+// number
+
+let roll: number = 899;
+
+// boolean
+let isAdmin: boolean = false;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null;
+
+
+//array
+
+let friends: string[] = ['Fahim', 'Babu', 'Munim'];
+
+let eligibleRolls: number[] = [911, 56, 917];
+
+//tuple --> Array -->must maintain the type order --> Type of values
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [50, 'Mr X', true];
