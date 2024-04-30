943c29ab039698d27a4b4b2832b01eda3db7251f
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-04-30 19:58:09 +0600

    reference type-object

---
+++ module1/src/1.5.ts
@@ -0,0 +1,20 @@
+//reference type --> object
+
+const user: {
+    company: 'Programming Hero'; //type --> literal type --> fixed value
+    readonly country: string;
+    firstName: string;
+    middleName?: string; // optional type
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'Programming Hero',
+    country: 'Bangladesh',
+    firstName: 'John',
+    middleName: 'Ulfi',
+    lastName: 'Doe',
+    isMarried: true
+};
+
+// user.country = "USA";  --> can't assign due to readonly type
+
