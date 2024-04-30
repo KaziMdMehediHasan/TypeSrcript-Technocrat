6d3fe9035b050bf14e4e465381a2e3517ce49ed7
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-05-01 00:08:21 +0600

    type alias

---
+++ module1/src/1.9.ts
@@ -0,0 +1,32 @@
+{
+    //Object Type Alias
+
+    type Student = {
+        name: string;
+        roll: number;
+        phone: string;
+        location?: string;
+    }
+    const student1: Student = {
+        name: 'Pritom',
+        roll: 1,
+        phone: "017XXXXXXXX",
+        location: 'Khulna, Bangladesh'
+    }
+
+    const student2: Student = {
+        name: 'Noyabit',
+        roll: 2,
+        phone: "016XXXXXXXX"
+    }
+
+    // function type alias
+
+    type Details = (name: string, roll: number, phone: string) => string;
+
+    const printStDetails: Details = (name, roll, phone) => `The details of ${name} are as follows: ${roll} and ${phone}`;
+
+    console.log(printStDetails(student1.name, student1.roll, student1.phone));
+
+    //
+}
